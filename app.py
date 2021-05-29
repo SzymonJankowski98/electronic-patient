0d852ecf5c24d73e79871441ad7178a3037703f6
SzymonJankowski98 <jankowski.szymon.98@gmail.com>
2021-05-29 12:06:28 +0200

    Show patients on index page

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template
+from fhirpy import SyncFHIRClient
 
 app = Flask(__name__)
+
+HAPI_BASE_URL = 'http://localhost:8080/baseR4'
+
+
+def get_patients():
+    client = SyncFHIRClient(HAPI_BASE_URL)
+    resources = client.resources('Patient').search()
+    patients = resources.fetch()
+    return patients
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    patients = get_patients()
+    return render_template('index.html', patients=patients)
 
 
 if __name__ == '__main__':
